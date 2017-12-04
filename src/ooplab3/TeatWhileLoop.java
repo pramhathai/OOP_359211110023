b655bb7ddd858dd2e6221b646abe6664f84e1f3e
Pramhathai <pramhathai2537@gmail.com>
2017-12-04 14:56:21 +0700

    Nested Loop

---
+++ src/ooplab3/TeatWhileLoop.java
@@ -0,0 +1,28 @@
+package ooplab3;
+
+public class TeatWhileLoop {
+
+    public static void main(String[] args) {
+        //while loop
+          int n = 1;
+//        while (n <= 10) {
+//
+//            System.out.println("Hello");
+//            n++;
+//
+//        }//while
+
+
+//        do-while
+        do {
+            System.out.println("Hello");
+            n++;
+
+        } while (n<=10);
+
+
+
+    }//main
+
+
+}//class
